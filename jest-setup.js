180dc3e0bf5e31c8deda79487e6de7571b5f3c47
Makeen Sabree <makeen.sabree@formidable.com>
2023-10-15 10:50:43 -0400

    chore: add comment for mocks

--- jest-setup.js
+++ jest-setup.js
@@ -1,1 +1,6 @@
 require('react-native-reanimated').setUpTests();
+
+// FIND THIS MOCK IN GESTURE HANDLER AND COMMENT OUT THE REST
+// jest.mock('./lib/commonjs/RNGestureHandlerModule', () =>
+//   require('./lib/commonjs/mocks')
+// );
